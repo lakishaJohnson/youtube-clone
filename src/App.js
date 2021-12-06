f506c5eb95e75ae4893bc91df5d960c6366c61fa
justin <justin.d.diaz@gmail.com>
2021-12-06 18:26:03 -0500

    some code for comments

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import Comments from "./components/comments";
+//import { Routes, Route, Redirect } from "react-router-dom";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <Comments />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Comments />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/comments.js
+++ src/components/comments.js
@@ -1,33 +1,49 @@
 import { Component } from "react";
-import { link } from "react-router-dom";
 
 class Comments extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       name: "",
-      Comment: "",
+      comment: "",
+      previousComments: [],
     };
   }
 
-  //   submitComment = () => {
-  //       if (this.state.name.length )
-  //   };
+  submitComment = (event) => {
+    event.preventDefault();
+    this.setState({
+      comment: [...this.state.comment, this.state.name],
+    });
+  };
+
+  handleInput = (event) => {
+    this.setState({
+      name: event.target.value,
+      comments: event.target.value,
+    });
+  };
 
   render() {
     const displayComment = () => {
-      <div>
-        <h2>{this.name}</h2>
-        <p>{this.Comment}</p>
-      </div>;
+      this.state.comment.map((word) => {
+        return (
+          <div>
+            <h2>{this.handleInput}</h2>
+            <p>{this.state.comment}</p>
+          </div>
+        );
+      });
     };
 
     return (
       <div>
-        <lablel for="name">Name</lablel>
-        <input type="text" id="name" value={this.state.name}></input>
-        <lablel for="comment">Comment</lablel>
-        <input type="text" id="comment" value={this.state.Comment}></input>
+        {console.log("this is this state name   " + this.state.name)}
+        {console.log(this.handleInput)}
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" onChange={this.handleInput}></input>
+        <label htmlFor="comment">Comment</label>
+        <input type="text" id="comment" onChange={this.handleInput}></input>
         <button onClick={this.submitComment}>Submit</button>
         {displayComment}
       </div>
