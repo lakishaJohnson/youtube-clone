cd93bfe1421b33b60742c81067a544dac98ada59
Lakisha J <lakishajohnson@pursuit.org>
2021-12-07 11:05:17 -0500

    Modified Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,42 @@
 import React from "react";
 
+
 class Home extends React.Component {
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
 
     this.state = {
       userInput: "",
     }
   }
 
-  onClick = (event) => {
+  handleUserInput = (event) => {
     this.setState({
       userInput: event.target.value,
     })
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      selectedVideo: event.target.value
+    })
+  }
+
   render() {
+
     return (
       <div>
-        <input type="text" placeholder="search..." id="search" />
-        <button onClick={this.onClick}>Search</button>
-        <p>{this.state.userInput}</p>
+        <form>
+          <input 
+        type="text" 
+        placeholder="search..." 
+        id="search" 
+        value={this.state.userInput}
+        onChange={this.handleUserInput} />
+        <button onSubmit={this.handleSubmit}>Search</button>
+        {/* <p>{this.state.userInput}</p> */}
+        </form>
       </div>
     );
   }
