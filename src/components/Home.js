5da97a07463d15774bd79249f51fc6e6f50b7c48
Lakisha J <lakishajohnson@pursuit.org>
2021-12-07 20:24:50 -0500

    Added fetch to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import VideoCard from "./VideoCard";
 
 
 class Home extends React.Component {
@@ -7,6 +8,8 @@ class Home extends React.Component {
 
     this.state = {
       userInput: "",
+      results: [],
+      videos: []
     }
   }
 
@@ -19,24 +22,46 @@ class Home extends React.Component {
   handleSubmit = (event) => {
     event.preventDefault()
     this.setState({
-      selectedVideo: event.target.value
+      results: this.state.userInput 
+    })
+    this.fetchVideos()
+  }
+  
+  
+
+  fetchVideos() {
+    // console.log("Success")
+    if(this.state.userInput === 0) return;
+    
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${this.state.userInput}&type=video&key=${process.env.REACT_APP_KEY}`)
+    .then(res => res.json())
+    .then((data) => {
+       console.log(data)
+      this.setState({
+        videos: data.items
+      })
     })
   }
+    
 
   render() {
-
+    const videosToDisplay = this.state.videos.map((video, i) => {
+      return (<VideoCard vid={video} key={i} />);
+    });
+    
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input 
         type="text" 
         placeholder="search..." 
         id="search" 
         value={this.state.userInput}
         onChange={this.handleUserInput} />
-        <button onSubmit={this.handleSubmit}>Search</button>
-        {/* <p>{this.state.userInput}</p> */}
+        <button type="submit">Search</button>
+        <p>{this.state.results}</p>
         </form>
+        {videosToDisplay}
       </div>
     );
   }
