461ac6b3753f668677143fb1a5cce25b8867beb9
Tashawnee Guarriello <tashawneeguarriello@pursuit.org>
2021-12-09 14:11:05 -0500

    Merges

--- src/App.js
+++ src/App.js
@@ -10,24 +10,26 @@ import About from "./components/About";
 import Comments from "./components/comments";
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
       youtubeAPI: [],
-    }
+    };
   }
-  
+
   componentDidMount() {
     // let userInput = this.state.userInput
-    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${this.props.userInput}&type=video&key=${process.env.REACT_APP_KEY}`)
-    .then(res => res.json())
-    .then((data) => {
-      // console.log(data)
-      this.setState({
-        youtubeAPI: data.snippet
-      })
-    })
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${this.props.userInput}&type=video&key=${process.env.REACT_APP_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data)
+        this.setState({
+          youtubeAPI: data.snippet,
+        });
+      });
   }
 
   render() {
@@ -40,12 +42,6 @@ class App extends React.Component {
           <Route exact path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
-<<<<<<< HEAD
-        <Comments />
-=======
-
-        
->>>>>>> b0ce38e1a986b6054786a710c642da797e273ed2
       </div>
     );
   }
--- src/components/comments.js
+++ src/components/comments.js
@@ -1,76 +1,46 @@
-import { Component } from "react";
+import React, { PureComponent, useState } from "react";
+import data from "./data.json";
+import { CommentSection } from "react-comments";
+import "react-comments/dist/index.css";
+import "./App.css";
 
-class Comments extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      input: {
-        name: "",
-        comment: "",
-      },
-      previousComments: [],
-    };
-  }
-
-  submitComment = (event) => {
-    event.preventDefault();
-    const { previousComments, input } = this.state;
-    this.setState({
-      previousComments: [...previousComments, input],
-      input: {
-        name: "",
-        comment: "",
-      },
-    });
+class App extends PureComponent {
+  state = {
+    comment: data,
   };
 
-  handleInput = (event) => {
-    const { input } = this.state;
-    this.setState({
-      input: { ...input, [event.target.name]: event.target.value },
-    });
+  handleState = (value) => {
+    this.setState({ comment: value });
   };
 
   render() {
-    const { previousComments, input } = this.state;
+    const userId = "01a";
+    const avatarUrl = "https://ui-avatars.com/api/name=Riya&background=random";
+    const name = "xyz";
+    const signinUrl = "/signin";
+    const signupUrl = "/signup";
+    let count = 0;
+    this.state.comment.map((i) => {
+      count += 1;
+      i.replies && i.replies.map((i) => (count += 1));
+    });
 
     return (
-      <div>
-        {console.log("this.state.name --> " + this.state.input.name)}
-        {console.log("this.state.comment --> " + this.state.input.comment)}
-        {console.log(this.state.previousComments)}
-        <label>Name</label>
-        <input
-          type="text"
-          name="name"
-          onChange={this.handleInput}
-          placeholder=" Name..."
-          value={input.name}
-        ></input>
-        <label>Comment</label>
-        <textarea
-          type="text"
-          name="comment"
-          onChange={this.handleInput}
-          placeholder=" ..."
-          value={input.comment}
-        ></textarea>
-        <button onClick={this.submitComment}>Submit</button>
-        <ul>
-          {previousComments.map((eachComment, index) => {
-            return (
-              <li key={index}>
-                <ul className="video-comment">
-                  <b>{eachComment.name}</b>
-                  <p>{eachComment.comment}</p>
-                </ul>
-              </li>
-            );
-          })}
-        </ul>
+      <div className="commentSection">
+        <div className="header">{count} Comments</div>
+
+        <CommentSection
+          currentUser={
+            userId && { userId: userId, avatarUrl: avatarUrl, name: name }
+          }
+          commentsArray={this.state.comment}
+          setComment={this.handleState}
+          signinUrl={signinUrl}
+          signupUrl={signupUrl}
+        />
       </div>
     );
   }
 }
 
-export default Comments;
+export default App;
